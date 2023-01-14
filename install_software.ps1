be6da8185b1c4749707a0bae87dc45bfe41c0b5e
yuchenfei <cf.yu@qq.com>
2023-01-14 18:34:30 +0800

    add software to app list

--- install_software.ps1
+++ install_software.ps1
@@ -10,6 +10,8 @@ $apps = @(
     "Bandisoft.Honeyview",
     "Daum.PotPlayer",
     "Bitwarden.Bitwarden",
+    "Ghisler.TotalCommander",
+    "SoftDeluxe.FreeDownloadManager",
     "PicGo.PicGo",
     "9P1WXPKB68KX", # Snipaste
     "9PKTQ5699M62", # iCloud
@@ -17,24 +19,29 @@ $apps = @(
     "Tencent.WeChat",
     "ByteDance.Feishu",
     "NetEase.MailMaster",
-    "SoftDeluxe.FreeDownloadManager",
     "EuSoft.Eudic", # 欧路词典
+    "Notion.Notion",
+    "Obsidian.Obsidian",
+    "EdrawSoft.MindMaster",
     # Game
     "Nvidia.GeForceExperience",
-    "BlueStack.BlueStacks"
+    "Valve.Steam"
 )
 
-function Install-Software {
-    Write-Host "Installing Software" -ForegroundColor Green
-    foreach ($app in $apps) {
-        $queryApp = winget list -e -q $app
-        if (![string]::Join("", $queryApp).contains($app)) {
-            Write-Host ("Installing: " + $app) -ForegroundColor Green
-            winget install -e --id $app --accept-package-agreements --accept-source-agreements --force
-            Write-Host $_
-        }
-        else {
-            Write-Host ("Skipping: " + $app + " (already installed)") -ForegroundColor Yellow
-        }
+Write-Host "Installing Software" -ForegroundColor Green
+foreach ($app in $apps) {
+    $queryApp = winget list -e -q $app
+    if (![string]::Join("", $queryApp).contains($app)) {
+        Write-Host ("Installing: " + $app) -ForegroundColor Green
+        winget install -e --id $app --accept-package-agreements --accept-source-agreements --force
+        Write-Host $_
+    }
+    else {
+        Write-Host ("Skipping: " + $app + " (already installed)") -ForegroundColor Yellow
     }
 }
+
+# Install Scoop Scftware
+scoop install geekuninstaller
+scoop install spacesniffer
+scoop install runcat
