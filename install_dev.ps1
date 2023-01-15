2a5db9014e7acadd97faedbade8423e8bb5abeb7
yuchenfei <yuchenfei19950319@gmail.com>
2023-01-15 17:53:28 +0800

    实现开发环境安装脚本

--- install_dev.ps1
+++ install_dev.ps1
@@ -1,5 +1,37 @@
-# python
+# ---------------- Util Function ----------------
 
-# node.js
+function Check-Command($cmdname) {
+    return [bool](Get-Command -Name $cmdname -ErrorAction SilentlyContinue)
+}
+
+function Update-EnvPath {
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", "Machine") `
+        + ";" + [System.Environment]::GetEnvironmentVariable("Path", "User")
+}
+
+# ---------------- Main Script ----------------
+
+# python
+if (!(Check-Command "conda")) {
+    winget install -e --id Anaconda.Miniconda3
+}
 
 # java
+if (!(Check-Command "javac")) {
+    winget install -e --id Oracle.JDK.17
+}
+
+# node.js
+if (!(Check-Command "fnm")){
+    # Install fnm
+    scoop install fnm
+    # Config profile for fnm
+    $config = "fnm env --use-on-cd | Out-String | Invoke-Expression"
+    $content = Get-Content $PROFILE
+    if (!($content.contains($config))){
+        Add-Content $PROFILE ("`n" + $config)
+    }
+    Update-EnvPath
+    # Install lts node
+    fnm install --lts
+}
