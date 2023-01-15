36f8b7980d5ef3b862eba4db7a5c6320e9adffd1
yuchenfei <yuchenfei19950319@gmail.com>
2023-01-15 17:50:33 +0800

    修改、完善脚本
    新系统运行需要接受协议
    输出执行流程
    完善 oh My Posh 安装流程

--- install_base.ps1
+++ install_base.ps1
@@ -1,6 +1,6 @@
 # ---------------- Util Function ----------------
 
-function query_app_existed($app_id) {
+function Find-AppExisted($app_id) {
     $query = winget list -e -q $app_id
     if ([string]::Join("", $query).contains($app_id)) {
         return $true
@@ -11,14 +11,12 @@ function query_app_existed($app_id) {
 }
 
 function Install-App($app_id) {
-    if (!(query_app_existed($app_id))) {
+    if (!(Find-AppExisted($app_id))) {
         Write-Host ("Installing: " + $app_id) -ForegroundColor Green
         winget install -e --id $app_id
-        return $true
     }
     else {
         Write-Host ("Skipping: " + $app_id + " (already installed)") -ForegroundColor Yellow
-        return $false
     }
 }
 
@@ -29,17 +27,23 @@ function Update-EnvPath {
 
 # ---------------- Main Script ----------------
 
+# Accept source agreements
+winget upgrade --accept-source-agreements
+
 # Install Developer Tools
-Install-App "Git.Git" | Out-Null
-Install-App "Microsoft.VisualStudioCode" | Out-Null
+Install-App "Git.Git"
+Install-App "Microsoft.VisualStudioCode"
 
 # Install oh My Posh
-if (Install-App ("JanDeDobbeleer.OhMyPosh")) {
+if (!(Find-AppExisted("JanDeDobbeleer.OhMyPosh"))) {
+    Write-Host ("Installing: oh My Posh") -ForegroundColor Green
+    winget install -e --id JanDeDobbeleer.OhMyPosh
     Update-EnvPath
     # Install font
     oh-my-posh font install Meslo
     # Install posh-git
-    PowerShellGet\Install-Module posh-git -Scope CurrentUser -Force
+    Install-PackageProvider NuGet -Force
+    Install-Module posh-git -Scope CurrentUser -Force
     Add-PoshGitToProfile
     # Config profile
     Add-Content $PROFILE "`noh-my-posh init pwsh | Invoke-Expression"
